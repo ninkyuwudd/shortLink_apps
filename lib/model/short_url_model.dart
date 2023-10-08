32daa079e4838b31eaeb828361296f4c0e29db37
Reihan Wudd Hibatullah <116327302+ninkyuwudd@users.noreply.github.com>
2023-10-08 13:29:46 +0700

    add sqlife service & provider

--- lib/model/short_url_model.dart
+++ lib/model/short_url_model.dart
@@ -1,21 +1,32 @@
 // To parse this JSON data, do
-//
-//     final welcome = welcomeFromJson(jsonString);
 
+//   final DataLink = DataLinkFromJson(jsonString);
 import 'dart:convert';
 
-Welcome welcomeFromJson(String str) => Welcome.fromJson(json.decode(str));
+final String tableLink = "shortLink";
 
-String welcomeToJson(Welcome data) => json.encode(data.toJson());
+class LinkFields {
+  static final List<String> values = [id, title, date, shortUrl, longUrl];
 
-class Welcome {
-  String id;
+  static final String id = 'id';
+  static final String title = 'title';
+  static final String date = 'date';
+  static final String shortUrl = 'shortUrl';
+  static final String longUrl = 'longUrl';
+}
+
+DataLink dataLinkFromJson(String str) => DataLink.fromJson(json.decode(str));
+
+String dataLinkToJson(DataLink data) => json.encode(data.toJson());
+
+class DataLink {
+  int? id;
   String title;
   String date;
   String shortUrl;
   String longUrl;
 
-  Welcome({
+  DataLink({
     required this.id,
     required this.title,
     required this.date,
@@ -23,7 +34,7 @@ class Welcome {
     required this.longUrl,
   });
 
-  factory Welcome.fromJson(Map<String, dynamic> json) => Welcome(
+  factory DataLink.fromJson(Map<String, dynamic> json) => DataLink(
         id: json["id"],
         title: json["title"],
         date: json["date"],
@@ -38,4 +49,17 @@ class Welcome {
         "shortUrl": shortUrl,
         "longUrl": longUrl,
       };
+
+  DataLink copy(
+          {int? id,
+          String? title,
+          String? date,
+          String? shortUrl,
+          String? longUrl}) =>
+      DataLink(
+          id: id ?? this.id,
+          title: title ?? this.title,
+          date: date ?? this.date,
+          shortUrl: shortUrl ?? this.shortUrl,
+          longUrl: longUrl ?? this.longUrl);
 }
